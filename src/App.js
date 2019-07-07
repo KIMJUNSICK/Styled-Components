92bfd8906de3e4f97eb1ebd1ca5554eff231014c
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-07 14:57:37 +0900

    Core of Styled Components
    Not Adding CSS Files / No module bundler for SASS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React, { Component, Fragment } from "react";
-import "./App.css";
+import React, { Component } from "react";
+import styled from "styled-components";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <button className="btn btn-success">CLICK!</button>
-        <button className="btn btn-fail">CLICK!</button>
-      </Fragment>
+      <Container>
+        <Button>HELLO</Button>
+        <Button danger>JUNSIK</Button>
+      </Container>
     );
   }
 }
+
+const Container = styled.div`
+  background-color: #e7e9bb;
+  height: 100vh;
+  width: 100%;
+`;
+
+const Button = styled.button`
+  border-radius: 5px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  background-color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#26D0CE" : "#1A2980")};
+`;
 
 export default App;
