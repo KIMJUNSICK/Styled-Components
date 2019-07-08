7f28f440f5933468478acb881cfb429fc5ef5962
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-08 12:07:02 +0900

    Theme

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, { Component, Fragment } from "react";
-import styled, {
-  GlobalStyleComponent,
-  css,
-  keyframes
-} from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Input placeholder="Something you want" />
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
     );
   }
 }
 
-const cssBlock = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.dangerColor};
+`;
+const Card = styled.div`
+  background-color: ${props => props.theme.mainColor};
 `;
 
-const Input = styled.input.attrs({
-  required: true
-})`
-  border: none;
-  ${cssBlock}
-`;
+const Form = () => (
+  <Card>
+    <Button>JUNSIK</Button>
+  </Card>
+);
 
 const Container = styled.div`
   background-color: #e7e9bb;
