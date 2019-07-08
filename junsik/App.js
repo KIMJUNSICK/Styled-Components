c26dd0e165635de74ae50dc36b2e4b32622e3cbc
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-08 12:49:42 +0900

    CSS on React Native

--- junsik/App.js
+++ junsik/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import styled from "styled-components";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Title>
+          junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!junsik!
+        </Title>
+      </Container>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 70px;
+`;
+
+const Title = styled.Text`
+  font-weight: 600;
+  font-size: 30px;
+`;
