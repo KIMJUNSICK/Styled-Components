942052aa383d6b0c24ac7e8cb3dfa1dcc07a4bb3
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-08 10:51:55 +0900

    Extra Attributes and Mixins

--- src/App.js
+++ src/App.js
@@ -1,70 +1,38 @@
 import React, { Component, Fragment } from "react";
-import styled, { createGlobalStyle, css, keyframes } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
- body {
-   padding: 0;
-   margin: 0;
- }
-`;
+import styled, {
+  GlobalStyleComponent,
+  css,
+  keyframes
+} from "styled-components";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <GlobalStyle />
-        <Container>
-          <Button>HELLO</Button>
-          <Button danger rotationTime={5}>
-            JUNSIK
-          </Button>
-          <Anchor href="http://google.com">Road to G.O.D</Anchor>
-        </Container>
-      </Fragment>
+      <Container>
+        <Input placeholder="Something you want" />
+      </Container>
     );
   }
 }
+
+const cssBlock = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`;
+
+const Input = styled.input.attrs({
+  required: true
+})`
+  border: none;
+  ${cssBlock}
+`;
 
 const Container = styled.div`
   background-color: #e7e9bb;
   height: 100vh;
   width: 100%;
-`;
-
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  background-color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-  background-color: ${props => (props.danger ? "#26D0CE" : "#1A2980")};
-  ${props => {
-    if (props.danger) {
-      return css`
-        animation: ${rotation} ${props.rotationTime}s linear infinite;
-      `;
-    }
-  }}
-`;
-
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration: none;
-`;
-
-const rotation = keyframes`
- from  {
-   transform : rotate(0deg)
- }
- to {
-  transform : rotate(360deg)
- } 
 `;
 
 export default App;
